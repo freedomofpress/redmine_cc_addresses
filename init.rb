a89e80a404307457fd7e6431359fe2900e2f6564
Jens Kraemer <jk@jkraemer.net>
2019-07-22 13:45:54 +0800

    reorganize Readme a bit, update information, bump plugin version to 0.5

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_cc_addresses do
   author_url 'http://peelman.us'
   url 'https://github.com/ageis/redmine_cc_addresses'
   description 'Allows CC addresses to be attached to an issue'
-  version '0.4.0'
+  version '0.5.0'
 
   project_module :cc_addresses do |map|
     map.permission :edit_cc_addresses, { :cc_addresses => [:create, :destroy] }
